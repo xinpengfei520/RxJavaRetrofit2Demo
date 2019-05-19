c56fcd5760767ded869a49eb650060f4117a27c2
xinpengfei <542270559@qq.com>
2019-05-19 18:35:20 +0800

    test

--- app/src/main/java/com/xpf/rxjavaretrofit2demo/ui/operator/RxJavaOperatorActivity.java
+++ app/src/main/java/com/xpf/rxjavaretrofit2demo/ui/operator/RxJavaOperatorActivity.java
@@ -10,9 +10,15 @@ import com.xpf.rxjavaretrofit2demo.utils.LogUtil;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 import butterknife.ButterKnife;
 import io.reactivex.Observable;
+import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.functions.Consumer;
 
 public class RxJavaOperatorActivity extends AppCompatActivity {
@@ -35,47 +41,60 @@ public class RxJavaOperatorActivity extends AppCompatActivity {
 
 
         //createObservable();
-
-        helloWorld();
+        //iterable();
+        fromFuture();
     }
 
-    private void helloWorld() {
+    private void fromFuture() {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        Future<Integer> future = executorService.submit(new MyCallable());
+
+        Observable.fromFuture(future, 2, TimeUnit.SECONDS)
+                .subscribe(s -> LogUtil.i(TAG, "任务运行的结果：" + s)
+                        , throwable -> LogUtil.e(TAG, "onError:" + throwable.getMessage()));
+    }
+
+    private void iterable() {
         List<Integer> list = new ArrayList<>();
         for (int i = 0; i < 6; i++) {
             list.add(i);
         }
 
-        Observable.just(list)
-                .subscribe(new Consumer<List>() {
-                    @Override
-                    public void accept(List list) throws Exception {
-                        LogUtil.i(TAG, "just():" + Arrays.toString(list.toArray()));
-                    }
-                });
+        Observable.just(list).subscribe((Consumer<List>) list1 -> LogUtil.i(TAG, "just():" + Arrays.toString(list1.toArray())));
 
-        Observable.fromIterable(list)
-                .subscribe(new Consumer<Integer>() {
-                    @Override
-                    public void accept(Integer integer) throws Exception {
-                        LogUtil.i(TAG, "fromIterable():" + integer);
-                    }
-                });
+        Observable.fromIterable(list).subscribe(integer -> LogUtil.i(TAG, "fromIterable():" + integer));
+
+        Observable.just(1, 2, 3, 4, 5).subscribe(integer -> LogUtil.i(TAG, "just():" + integer));
     }
 
     private void createObservable() {
-//        Observable.create((ObservableOnSubscribe<Integer>) emitter -> {
-//            try {
-//                if (!emitter.isDisposed()) {
-//                    for (int i = 0; i < 10; i++) {
-//                        emitter.onNext(i);
-//                    }
-//                    emitter.onComplete();
-//                }
-//            } catch (Exception e) {
-//                emitter.onError(e);
-//            }
-//        }).subscribe(integer -> LogUtil.i(TAG, "onNext():" + integer),
-//                throwable -> LogUtil.i(TAG, "onError():" + throwable.getMessage()),
-//                () -> LogUtil.i(TAG, "onComplete()"));
+        Observable.create((ObservableOnSubscribe<Integer>) emitter -> {
+            try {
+                if (!emitter.isDisposed()) {
+                    for (int i = 0; i < 10; i++) {
+                        emitter.onNext(i);
+                    }
+                    emitter.onComplete();
+                }
+            } catch (Exception e) {
+                emitter.onError(e);
+            }
+        }).subscribe(integer -> LogUtil.i(TAG, "onNext():" + integer),
+                throwable -> LogUtil.i(TAG, "onError():" + throwable.getMessage()),
+                () -> LogUtil.i(TAG, "onComplete()"));
+    }
+
+    static class MyCallable implements Callable<Integer> {
+
+        @Override
+        public Integer call() throws Exception {
+            LogUtil.i(TAG, "call():在这里模拟一些耗时操作...");
+            Thread.sleep(3000);
+            int sum = 0;
+            for (int i = 0; i <= 100; i++) {
+                sum += i;
+            }
+            return sum;
+        }
     }
 }
