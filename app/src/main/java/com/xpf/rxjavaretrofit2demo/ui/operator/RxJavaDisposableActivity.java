122fc569923e55766248a00669809385f2282b9f
xinpengfei <542270559@qq.com>
2019-06-02 14:39:10 +0800

    RxJavaDisposable

--- app/src/main/java/com/xpf/rxjavaretrofit2demo/ui/operator/RxJavaDisposableActivity.java
+++ app/src/main/java/com/xpf/rxjavaretrofit2demo/ui/operator/RxJavaDisposableActivity.java
@@ -2,20 +2,33 @@ package com.xpf.rxjavaretrofit2demo.ui.operator;
 
 import android.os.Bundle;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import com.trello.rxlifecycle3.android.RxLifecycleAndroid;
+import com.trello.rxlifecycle3.components.support.RxAppCompatActivity;
 import com.xpf.rxjavaretrofit2demo.R;
+
+import io.reactivex.Observable;
 
 /**
  * Created by x-sir on 2019-06-02 :)
  * Function:RxJava 2.x Disposable
  * {@link # https://github.com/xinpengfei520/RxJavaRetrofit2Demo}
  */
-public class RxJavaDisposableActivity extends AppCompatActivity {
+public class RxJavaDisposableActivity extends RxAppCompatActivity {
+
+    private Observable<Integer> myObservable = Observable.range(0, 25);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rx_java_disposable);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        myObservable
+                .compose(RxLifecycleAndroid.bindActivity(lifecycle()))
+                //.compose(RxLifecycle.bindUntilEvent(lifecycle(), ActivityEvent.DESTROY))
+                .subscribe();
     }
 }
